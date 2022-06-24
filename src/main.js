872da13829bfaefb50e282c075f7137643f276ec
Alessio Forti <info@alessioforti.it>
2022-06-24 21:36:35 +0200

    Setup Firebase

--- src/main.js
+++ src/main.js
@@ -1,3 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA4JP1p2Bm8lPyz-h5CFOCfFU94R7dIMIY",
+  authDomain: "simple-firebase-admin.firebaseapp.com",
+  projectId: "simple-firebase-admin",
+  storageBucket: "simple-firebase-admin.appspot.com",
+  messagingSenderId: "808303756377",
+  appId: "1:808303756377:web:98b16072cf13ed083b5d48"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 import { createApp } from 'vue'
 import App from './App.vue'
 
